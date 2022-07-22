5ccc0f6b28b14d7ffcc924daf6951c09198797d9
MarteCodes <jeanmarte93@gmail.com>
2022-07-22 11:44:49 -0400

    practice

--- challenges.js
+++ challenges.js
@@ -85,6 +85,59 @@ function maxSubarraySum(arr, num) {
   return total;
 }
 
-function minSubArrayLen (arr, num) {
-  
+function minSubArrayLen (nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    
+    // if current window doesn't add up to the given sum then 
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
+
+    console.log(total);
+  }
+ 
+  return minLen === Infinity ? 0 : minLen;
+
+}
+
+function findLongestSubstring(str){
+  // add whatever parameters you deem necessary - good luck!
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    longest = Math.max(longest, i - start + 1);
+
+    seen[char]= i + 1;
+
+    console.log(seen)
+  }
+
+  return longest;
+
 }
